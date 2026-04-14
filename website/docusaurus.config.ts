195258da602ab3bcddb2410357800aabf78c04de
Al Mahdi <almahdi404@proton.me>
2026-04-14 18:02:04 +0600

    website: add algolia search

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import type { Options } from "@docusaurus/plugin-content-docs";
 import type * as PresetClassic from "@docusaurus/preset-classic";
 import type { Config } from "@docusaurus/types";
@@ -102,7 +103,6 @@ const config: Config = {
       } satisfies Options,
     ],
   ],
-
   themeConfig: {
     // Replace with your project's social card
     image: "img/og-image.png",
@@ -141,6 +141,32 @@ const config: Config = {
           position: "right",
         },
       ],
+    },
+    algolia: {
+      appId: process.env.ALGO_APP_ID || "",
+      apiKey: process.env.ALGO_SEARCH_API_KEY || "",
+      indexName: process.env.ALGO_INDEX_NAME || "",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: "/docs/", // or as RegExp: /\/docs\//
+      //   to: "/",
+      // },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      // searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
     },
     footer: {
       copyright: `<a href="https://github.com/cngpac/cngpac/blob/main/LICENSE">Apache 2.0</a> | Copyright © ${new Date().getFullYear()} Cngpac. Built with Docusaurus.`,
